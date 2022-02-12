d71d164794473eac2c3d1f353df4e0e282c713e6
Oleksandr Leiberiuk <Lejberyk.ua@gmail.com>
2022-02-12 17:37:54 +0200

    created Mongo DB, the server starts in local

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const contactsRouter = require('./routes/api/contacts');
-const usersRouter = require('./routes/api/users');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+// const contactsRouter = require('./routes/api/contacts');
+// const usersRouter = require("./routes/api/users");
 
 const app = express();
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-
-dotenv.config(); // запись содержимого из .env (ключ доступа) в process.env
-const { DB_HOST } = process.env;
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    console.log('Database connection successful');
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  });
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json()); // нужно для отправки тела запроса в формате json
-app.use(express.static('public')); // разрешает Express раздачу статичных файлов из папки public
+// app.use(express.static("public")); // разрешает Express раздачу статичных файлов из папки public
 
-app.use('/api/contacts', contactsRouter);
-app.use('/api/users', usersRouter);
+// app.use("/api/users", usersRouter);
+// app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((error, req, res, next) => {
-  const { status = 500, message = 'Server error' } = error;
+  const { status = 500, message = "Server error" } = error;
   res.status(status).json({ message });
 });
 
---
+++ models/transaction.js
@@ -0,0 +1,96 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const transactionSchema = Schema({
+  // Блок дат
+  date: {
+    type: Date,
+    min: "2020-01-01",
+  },
+  year: {
+    type: Date, //???
+  },
+  month: {
+    type: Date, //???
+  },
+  // Тип транзакции доход=true, расход=false
+  typeTransaction: {
+    type: Boolean,
+    default: false,
+  },
+  // Категории расходов
+  category: {
+    type: String,
+    enum: [
+      "basic expenses",
+      "food",
+      "car",
+      "me",
+      "children",
+      "home",
+      "education",
+      "leisure",
+      "other expenses",
+    ],
+    default: "basic expenses",
+  },
+  // Комментарий к доходу
+  сommentary: {
+    type: String,
+    // required: [true],
+  },
+  // Сумма транзакции
+  amountTransaction: {
+    type: Number,
+  },
+  // Баланс счета
+  balance: {
+    type: Number,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
+
+// Joi схема для записи дохода
+const earningsSchema = Joi.object({
+  date: Joi.date().required(),
+  year: Joi.date().required(),
+  month: Joi.date().required(),
+  typeTransaction: Joi.boolean().required(),
+  сommentary: Joi.string(),
+  amountTransaction: Joi.number().required(),
+  balance: Joi.number().required(),
+});
+
+// Joi схема для записи расходов
+const expenseSchema = Joi.object({
+  date: Joi.date().required(),
+  year: Joi.date().required(),
+  month: Joi.date().required(),
+  typeTransaction: Joi.boolean().required(),
+  category: Joi.string().required(),
+  amountTransaction: Joi.number().required(),
+  balance: Joi.number().required(),
+});
+
+// Joi схема статистики для дашборда(таблица, график)
+const statisticSchema = Joi.object({
+  date: Joi.date().required(),
+  year: Joi.date().required(),
+  month: Joi.date().required(),
+  typeTransaction: Joi.boolean().required(),
+  category: Joi.string().required(),
+  amountTransaction: Joi.number().required(),
+  balance: Joi.number().required(),
+});
+
+const Transaction = model("transaction", transactionSchema);
+
+module.exports = {
+  Transaction,
+  earningsSchema,
+  expenseSchema,
+  statisticSchema,
+};
